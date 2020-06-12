49aadc4ad53c6117d6a76672fdb36a4d5da5adea
Christina Harris <christinathedesigner@outlook.com>
2020-06-12 08:09:06 -0700

    adding in BTS Node with contains and insert

--- names/names.py
+++ names/names.py
@@ -1,5 +1,41 @@
 import time
 
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+
+        if self.value == value:
+            return
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BSTNode(value)
+        if value > self.value:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target < self.value and self.left:
+            return self.left.contains(target)
+        if target > self.value and self.right:
+            return self.right.contains(target)
+        return False
+
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
